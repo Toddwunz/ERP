78dfcedce85eda8376f7321d90871d9813cca4de
Todd Wu <toddwunz@gmail.com>
2019-06-27 02:06:12 +1200

    cash balance part

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TotalLab = new System.Windows.Forms.Label();
+            this.Ltotal = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BalanceBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TotalLab);
+            this.groupBox1.Controls.Add(this.Ltotal);
+            this.groupBox1.Location = new System.Drawing.Point(12, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(183, 411);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cash Calculation";
+            // 
+            // TotalLab
+            // 
+            this.TotalLab.AutoSize = true;
+            this.TotalLab.Location = new System.Drawing.Point(59, 395);
+            this.TotalLab.Name = "TotalLab";
+            this.TotalLab.Size = new System.Drawing.Size(0, 13);
+            this.TotalLab.TabIndex = 1;
+            // 
+            // Ltotal
+            // 
+            this.Ltotal.AutoSize = true;
+            this.Ltotal.Location = new System.Drawing.Point(6, 395);
+            this.Ltotal.Name = "Ltotal";
+            this.Ltotal.Size = new System.Drawing.Size(34, 13);
+            this.Ltotal.TabIndex = 0;
+            this.Ltotal.Text = "Total:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(285, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // BalanceBtn
+            // 
+            this.BalanceBtn.Location = new System.Drawing.Point(664, 415);
+            this.BalanceBtn.Name = "BalanceBtn";
+            this.BalanceBtn.Size = new System.Drawing.Size(75, 23);
+            this.BalanceBtn.TabIndex = 2;
+            this.BalanceBtn.Text = "Balence";
+            this.BalanceBtn.UseVisualStyleBackColor = true;
+            this.BalanceBtn.Click += new System.EventHandler(this.BalanceBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BalanceBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label TotalLab;
+        private System.Windows.Forms.Label Ltotal;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BalanceBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,64 @@ namespace ERP
         public Form1()
         {
             InitializeComponent();
+            int n = 9;
+            TextBox[] textBoxes = new TextBox[n];
+            Label[] labels = new Label[n];
+            String[] strnotes = new string[] { "100", "50", "20", "10", "5", "2", "1", "50c", "20c", "10c"};
+            for (int i = 0; i < n; i++)
+            {
+                labels[i] = new Label();
+                textBoxes[i] = new TextBox();
+                labels[i].Parent = groupBox1;
+                textBoxes[i].Parent = groupBox1;
+                labels[i].Location = new System.Drawing.Point(9, 20+i*40);
+                textBoxes[i].Location = new System.Drawing.Point(40, 20+i*40);
+                labels[i].Size = new Size(30, 20);
+                textBoxes[i].Size = new Size(100, 20);
+                labels[i].Text = strnotes[i];
+                textBoxes[i].Text = "";
+                textBoxes[i].TabIndex = i;
+                textBoxes[i].KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+                this.groupBox1.Controls.Add(textBoxes[i]);
+                this.groupBox1.Controls.Add(labels[i]);
+            }
+
         }
+
+        private void BalanceBtn_Click(object sender, EventArgs e)
+        {
+            float[] notes = { 100f, 50f, 20f, 10f, 5f, 2f, 1f, 0.5f, 0.2f, 0.1f };
+            float Total = 0;
+            foreach (Control TxtBox in this.groupBox1.Controls)
+            {
+                if (TxtBox is TextBox)
+                {
+                    if (TxtBox.Text == "") { TxtBox.Text = "0"; }
+                    float Value = float.Parse(TxtBox.Text);
+                    int TBindex = TxtBox.TabIndex;
+                    Total += Value * notes[TBindex];
+                }
+                TotalLab.Text = Total.ToString();
+            }
+
+        }
+
+        public void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8 && e.KeyChar != 9 && e.KeyChar != 13)
+            {
+                MessageBox.Show("请输入正确的数字");
+                this.textBox1.Text = "";
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                SendKeys.Send("{tab}");
+            }
+
+        }
+
     }
 }
